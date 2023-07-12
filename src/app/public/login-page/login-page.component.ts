5f04ba0cf4ce70f87d876630f62ce247ca158bd9
robert <78978033+robertGGA@users.noreply.github.com>
2023-07-13 02:08:44 +0300

    Added logic for login-page

--- src/app/public/login-page/login-page.component.ts
+++ src/app/public/login-page/login-page.component.ts
@@ -1,17 +1,49 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, OnInit,} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {RouterLink} from "@angular/router";
+import {AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {emailRegex} from "@core/utils/form-helper";
 
 @Component({
   selector: 'rg-login-page',
   standalone: true,
-  imports: [CommonModule, MatInputModule, MatButtonModule, RouterLink],
+  imports: [CommonModule, MatInputModule, MatButtonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.sass'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
 
+  loginGroup!: FormGroup;
+  private fb = inject(FormBuilder);
+
+  ngOnInit(): void {
+    this.loginGroup = this.fb.group({
+      email: new FormControl<string>('', [
+        Validators.required,
+        Validators.pattern(emailRegex)
+      ]),
+      password: new FormControl<string>('', [
+        Validators.required
+      ])
+    })
+  }
+
+  onSubmit(): void {
+    console.log(this.loginGroup.value);
+  }
+
+  get emailControl(): AbstractControl {
+    return this.loginGroup.get('email')!;
+  }
+
+  get passwordControl(): AbstractControl {
+    return this.loginGroup.get('password')!;
+  }
+
+  get hasNoErrors(): boolean {
+    return !(!this.passwordControl.errors && !this.emailControl.errors)
+  }
 }
