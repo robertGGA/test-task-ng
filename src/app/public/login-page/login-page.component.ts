0ee8fe6d2064c0e3273150afa61836663b742e76
robert <78978033+robertGGA@users.noreply.github.com>
2023-07-13 01:22:42 +0300

    Added login pages & styles

--- src/app/public/login-page/login-page.component.ts
+++ src/app/public/login-page/login-page.component.ts
@@ -1,10 +1,13 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'rg-login-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatInputModule, MatButtonModule, RouterLink],
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.sass'],
   changeDetection: ChangeDetectionStrategy.OnPush
--- src/app/shared/components/ui/header/header.component.ts
+++ src/app/shared/components/ui/header/header.component.ts
@@ -1,10 +1,11 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'rg-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass'],
   changeDetection: ChangeDetectionStrategy.OnPush
